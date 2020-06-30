a3dff09c35c7e5a2140cd7cccaccb5314859a133
ravinalawade <ravinalawade5@gmail.com>
2020-07-01 01:39:05 +0530

    deploying

--- server/settings.py
+++ server/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database',
+        'USER': 'ravi',
+        'PASSWORD': 'ravi',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
